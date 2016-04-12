d7ce485e6d61a1d8f8583678f20df3cfa2730c4d
oscarmarinmiro <oscarmarinmiro@github.com>
2016-04-12 03:56:15 +0200

    - updated aframe-stereo-component dist

--- dist/aframe-stereo-component.js
+++ dist/aframe-stereo-component.js
@@ -84,78 +84,103 @@
 	        mode: { type: 'string', default: "full"}
 	      },
 	       init: function(){
-	          var object3D = this.el.object3D;
 
+	          // Flag to acknowledge if 'click' on video has been attached to canvas
+	          // Keep in mind that canvas is the last thing initialized on a scene so have to wait for the event
+	          // or just check in every tick if is not undefined
 
+	          this.video_click_event_added = false;
+
+	          this.material_is_a_video = false;
+
+	          // Check if material is a video from html tag (object3D.material.map instanceof THREE.VideoTexture does not
+	          // always work
+
+	          if(this.el.getAttribute("material")!==null
+	              && 'src' in this.el.getAttribute("material")
+	              && document.querySelector(this.el.getAttribute("material").src)!== null
+	              && document.querySelector(this.el.getAttribute("material").src).tagName === "VIDEO"){
+
+	              this.material_is_a_video = true;
+	          }
+
+	          var object3D = this.el.object3D.children[0];
+
+	          // In A-Frame 0.2.0, objects are all groups so sphere is the first children
 	          // Check if it's a sphere w/ video material, and if so
+	          // Also note that in A-Frame 0.2.0 sphere entities are THREE.SphereBufferGeometry
 
-	          if(object3D.geometry instanceof THREE.SphereGeometry
-	                   && 'map' in object3D.material 
-	                   && object3D.material.map instanceof THREE.VideoTexture) {
-
-
-	              // hold video element for play on click (mobile)
-
-	              var self = this;
-
-	              // attach video element onclick to scene canvas
-
-	              this.videoEl = document.querySelector(this.el.components.material.textureSrc);
-
-	               this.el.sceneEl.canvas.onclick = function () {
-	                    self.videoEl.play();
-	               };
+	          if((object3D.geometry instanceof THREE.SphereGeometry || object3D.geometry instanceof THREE.SphereBufferGeometry)
+	                   && this.material_is_a_video) {
 
 	              // if half-dome mode, rebuild geometry (with default 100, radius, 64 width segments and 64 height segments)
 
-	              if(this.data.mode === "half"){
+	              if (this.data.mode === "half") {
 
-	                var geo_def = this.el.getAttribute("geometry");
-	                var geometry = new THREE.SphereGeometry(geo_def.radius || 100, geo_def.segmentsWidth || 64, geo_def.segmentsHeight || 64,  Math.PI/2, Math.PI, 0, Math.PI);
+	                  var geo_def = this.el.getAttribute("geometry");
+	                  var geometry = new THREE.SphereGeometry(geo_def.radius || 100, geo_def.segmentsWidth || 64, geo_def.segmentsHeight || 64, Math.PI / 2, Math.PI, 0, Math.PI);
 
-	                object3D.geometry = geometry;
+	              }
+	              else {
+	                  var geo_def = this.el.getAttribute("geometry");
+	                  var geometry = new THREE.SphereGeometry(geo_def.radius || 100, geo_def.segmentsWidth || 64, geo_def.segmentsHeight || 64);
+	              }
 
-	                // Rotate to put center of half panorama in front
+	              // Panorama in front
 
-	                object3D.rotation.y = -Math.PI / 2;
+	              object3D.rotation.y = Math.PI / 2;
 
-	                }
 
-	                // If left eye, take first horizontal half of texture from video
+	              // If left eye, take first horizontal half of texture from video
 
-	                if(this.data.eye === "left"){
+	              if (this.data.eye === "left") {
 
-	                      var uvs = object3D.geometry.faceVertexUvs[ 0 ];
-	                      for (var i = 0; i < uvs.length; i++) {
-	                          for (var j = 0; j < 3; j++) {
-	                              uvs[ i ][ j ].x *= 0.5;
-	                          }
+	                  var uvs = geometry.faceVertexUvs[ 0 ];
+	                  for (var i = 0; i < uvs.length; i++) {
+	                      for (var j = 0; j < 3; j++) {
+	                          uvs[ i ][ j ].x *= 0.5;
 	                      }
+	                  }
 
-	                }
+	              }
 
-	                // If right eye, take last horizontal half of texture from video
+	              // If right eye, take last horizontal half of texture from video
 
-	                if(this.data.eye === "right"){
-	                      var uvs = object3D.geometry.faceVertexUvs[ 0 ];
-	                      for (var i = 0; i < uvs.length; i++) {
-	                          for (var j = 0; j < 3; j++) {
-	                              uvs[ i ][ j ].x *= 0.5;
-	                              uvs[ i ][ j ].x += 0.5;                            
-	                          }
+	              if (this.data.eye === "right") {
+
+	                  var uvs = geometry.faceVertexUvs[ 0 ];
+	                  for (var i = 0; i < uvs.length; i++) {
+	                      for (var j = 0; j < 3; j++) {
+	                          uvs[ i ][ j ].x *= 0.5;
+	                          uvs[ i ][ j ].x += 0.5;
+
 	                      }
+	                  }
 
-	                }                             
+	              }
 
+	              // As AFrame 0.2.0 builds bufferspheres from sphere entities, transform
+	              // into buffergeometry for coherence
+
+	              object3D.geometry = new THREE.BufferGeometry().fromGeometry(geometry);
+
+	          }
+	          else{
+
+	              // No need to attach video click if not a sphere and not a video, set this to true
+
+	              this.video_click_event_added = true;
 
 	          }
 
-	       }, 
+
+	       },
+
 	       // On element update, put in the right layer, 0:both, 1:left, 2:right (spheres or not)
 
 	       update: function(oldData){
 
-	            var object3D = this.el.object3D;
+	            var object3D = this.el.object3D.children[0];
 	            var data = this.data;
 
 	            if(data.eye === "both"){
@@ -164,6 +189,34 @@
 	            else{
 	              object3D.layers.set(data.eye === 'left' ? 1:2);
 	            }
+
+	       },
+
+	       tick: function(time){
+
+	           // If this value is false, it means that (a) this is a video on a sphere [see init method]
+	           // and (b) of course, tick is not added
+
+	           if(!this.video_click_event_added){
+	                if(typeof(this.el.sceneEl.canvas) !== 'undefined'){
+
+	                   // Get video DOM
+
+	                   this.videoEl = this.el.object3D.children[0].material.map.image;
+
+	                   // On canvas click, play video element. Use self to not lose track of object into event handler
+
+	                   var self = this;
+
+	                   this.el.sceneEl.canvas.onclick = function () {
+	                      self.videoEl.play();
+	                   };
+
+	                   // Signal that click event is added
+	                   this.video_click_event_added = true;
+
+	                }
+	           }
 
 	       }
 	     },
@@ -176,16 +229,16 @@
 	        eye: { type: 'string', default: "left"}
 	      },
 
-	       // Since layer enabling for cam should be done on the right object down this object hierarchy,
+	       // Cam is not attached on init, so use a flag to do this once at 'tick'
+
 	       // Use update every tick if flagged as 'not changed yet'
 
 	       init: function(){
-	            // Register update on tick
-	            this.el.sceneEl.addBehavior(this);
+	            // Flag to register if cam layer has already changed
 	            this.layer_changed = false;
 	       },
 
-	       tick: function(oldData){
+	       tick: function(time){
 
 	            
 	            var originalData = this.data;
